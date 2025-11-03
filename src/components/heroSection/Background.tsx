609494a8cafba355c619b18cd8aa6ebadeb529c3
Sanskar Mishra <sanskarmishra479@gmail.com>
2025-11-04 01:55:40 +0530

    change background

--- src/components/heroSection/Background.tsx
+++ src/components/heroSection/Background.tsx
@@ -1,9 +1,10 @@
 import UnicornScene from "unicornstudio-react";
 
 export default function Background() {
+  const productId = "IhWuAwAT3WTs3PRzElTP"
   return (
     <div className="h-screen w-screen overflow-hidden">
-      <UnicornScene projectId="IhWuAwAT3WTs3PRzElTP" width={"100%"} height={"112%"} />
+      <UnicornScene projectId={`${{productId}}`} width={"100%"} height={"112%"} />
     </div>
   );
 }
