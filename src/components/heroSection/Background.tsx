68888eeaf9bf34cf7d57e5b31db26c9fda3de871
Sanskar Mishra <sanskarmishra479@gmail.com>
2025-11-04 01:59:21 +0530

    add env

--- src/components/heroSection/Background.tsx
+++ src/components/heroSection/Background.tsx
@@ -1,10 +1,10 @@
 import UnicornScene from "unicornstudio-react";
 
 export default function Background() {
-  const productId = "IhWuAwAT3WTs3PRzElTP"
+  const productId = import.meta.env.VITE_PRODUCT_ID;
   return (
     <div className="h-screen w-screen overflow-hidden">
-      <UnicornScene projectId={`${{productId}}`} width={"100%"} height={"112%"} />
+      <UnicornScene projectId={productId} width={"100%"} height={"112%"} />
     </div>
   );
 }
